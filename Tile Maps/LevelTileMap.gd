2765cb10bb74424b708fedc873daaf48ecab6a24
OwOPango <WarlinOG@gmail.com>
2025-02-23 18:15:17 -0500

    Initial commit

---
+++ Tile Maps/LevelTileMap.gd
@@ -0,0 +1,19 @@
+class_name LevelTileMap extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	LevelManager.ChangeTilemapBounds(GetTilemapBounds())
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func GetTilemapBounds() -> Array[Vector2]:
+	var bounds : Array[ Vector2 ] = []
+	bounds.append(
+		Vector2( get_used_rect().position * rendering_quadrant_size ) 
+	)
+	bounds.append(
+		Vector2( get_used_rect().end * rendering_quadrant_size)
+	)
+	return bounds
