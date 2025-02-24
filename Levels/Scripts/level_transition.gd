30e99ba8c61e17873c3fb0eb8f6ff5ac5a41063b
ellesper <emily@gruenei.ch>
2025-02-23 19:42:24 -0500

    Fixed LevelTransition bug

--- Levels/Scripts/level_transition.gd
+++ Levels/Scripts/level_transition.gd
@@ -93,7 +93,7 @@ func _update_area() -> void:
 		collision_shape = get_node("CollisionShape2D")
 		
 	
-	#collision_shape.shape.size = new_rect
+	collision_shape.shape.size = new_rect
 	collision_shape.position = new_position
 
 
