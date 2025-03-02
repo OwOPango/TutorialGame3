b40473cb3e7860731e87364ef555107969eff58d
OwOPango <WarlinOG@gmail.com>
2025-03-01 19:04:04 -0500

    new npc dialog bits

--- 00_Globals/global_save_manager.gd
+++ 00_Globals/global_save_manager.gd
@@ -12,7 +12,9 @@ var current_save : Dictionary = {
 		pos_y = 0
 	},
 	items = [],
-	persistence = [],
+	persistence = [
+		
+	],
 	quests = []
 }
 
@@ -63,3 +65,12 @@ func update_scene_path() -> void:
 
 func update_item_data() -> void:
 	current_save.items = PlayerManager.INVENTORY_DATA.get_save_data()
+	
+func add_persistent_value( value : String ) -> void:
+	if check_persistent_value( value ) == false:
+		current_save.persistence.append( value )
+	pass
+	
+func check_persistent_value( value : String ) -> bool:
+	var p = current_save.persistence as Array
+	return p.has( value )
