c0e4bbb6800516f59a5612dd35a7b7404ad0e79f
OwOPango <WarlinOG@gmail.com>
2025-04-13 15:55:03 -0400

    battle system v1

--- Battle/debug.gd
+++ Battle/debug.gd
@@ -10,5 +10,8 @@ func _unhandled_key_input(event: InputEvent) -> void:
 				get_tree().reload_current_scene()
 			KEY_Q:
 				get_tree().quit()
-		
+			KEY_F11:
+				var is_fullscreen: bool = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+				var target_mode: int = DisplayServer.WINDOW_MODE_WINDOWED if is_fullscreen else DisplayServer.WINDOW_MODE_FULLSCREEN
+				DisplayServer.window_set_mode(target_mode)
 		
